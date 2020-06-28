434506c8793969ea6b08446c88284a2a61a54cb8
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-28 12:42:46 +0300

    Catalogue - LoggingSystem started

--- FilesCatalogue/Data/CommonFile.cs
+++ FilesCatalogue/Data/CommonFile.cs
@@ -13,6 +13,94 @@ namespace FilesCatalogue.Data
         protected string _category;
         protected long _size;
 
-        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Name could not be empty!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public Guid ID
+        {
+            get
+            {
+                if (_guidCode.Equals(Guid.Empty) || string.IsNullOrEmpty(_guidCode.ToString()))
+                {
+                    return Guid.NewGuid();
+                }
+
+                return _guidCode;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value.ToString()))
+                {
+                    _guidCode = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You must set up a valid ID!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public long Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _size = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Size can not be negative and must be greater than 0!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public virtual string GetFileInformation()
+        {
+            string result = $"File type: {this.GetType()}\n" +
+                $"File name: {this._name}\n" +
+                $"File ID: {this._guidCode}\n" +
+                $"Category: {this._category}\n" +
+                $"File size: {this._size} bytes\n";
+            return result;
+        }
     }
 }
---
+++ FilesCatalogue/Logger/LoggingSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesCatalogue.Logger
+{
+    class LoggingSystem
+    {
+        private static LoggingSystem _instance;
+
+        private LoggingSystem()
+        {
+        }
+
+        public LoggingSystem GetLoggingSystemInstance()
+        {
+            if(_instance.Equals(null))
+            {
+                _instance = new LoggingSystem();
+            }
+
+            return _instance;
+        }
+
+        public void CreateLogFile()
+        {
+        }
+    }
+}
