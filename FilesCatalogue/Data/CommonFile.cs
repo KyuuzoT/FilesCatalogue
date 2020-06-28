e31fd6840f9a89e42109e57b1989cde2a25e3d45
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-28 12:30:02 +0300

    Added Data folder and classes for main file types in catalogue

---
+++ FilesCatalogue/Data/CommonFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesCatalogue.Data
+{
+    class CommonFile
+    {
+        protected string _name;
+        protected Guid _guidCode;
+        protected string _category;
+        protected long _size;
+
+        
+    }
+}
