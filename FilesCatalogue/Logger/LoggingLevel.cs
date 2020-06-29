668b9bc9ee0f388b2d247a665914775c3b11e83a
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-29 10:31:17 +0300

    Added Logging levels

---
+++ FilesCatalogue/Logger/LoggingLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesCatalogue.Logger
+{
+    enum LoggingLevel
+    {
+        Debug,
+        Info,
+        Error
+    }
+}
